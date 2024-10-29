7fda4c60a626f9deb54d472ffc91e62090865e32
Neto Russo <netoacrn1@gmail.com>
2024-10-29 20:54:14 -0300

    Ajustando estilos

--- src/components/Marcas/index.tsx
+++ src/components/Marcas/index.tsx
@@ -8,7 +8,7 @@ const Marcas = () => {
   return (
     <section className={Style.marcas} aria-label='Conheça as marcas do Grupo Mensch'>
       <div className={Style.marcas__detalhe} />
-      <h2 className={Style.marcas__titulo}>Conheça as marcas do Grupo Mensch</h2>
+      <h2 className={Style.marcas__titulo}>Conheça as marcas do <span>Grupo Mensch</span>.</h2>
       <div className={Style.marcas__slide} >
         <Slider />
       </div>
