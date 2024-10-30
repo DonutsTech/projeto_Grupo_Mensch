7db95c6574e9b907f1b64eaa2a5655fa5269556a
Neto Russo <netoacrn1@gmail.com>
2024-10-30 01:03:17 -0300

    Ajustando os estilos e adicionando o footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Menu from "@/components/Menu";
+import Footer from "@/components/Footer";
 
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body>
         <Menu />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import Image from 'next/image';
+import Style from './Footer.module.scss';
+import mensch from './logo_grupoMensch_white.svg';
+import donuts from './donuts_icon_original.svg';
+import Link from 'next/link';
+
+const Footer = () => {
+
+  return (
+    <footer className={Style.footer}>
+      <div className={Style.footer__box} >
+        <div className={Style.footer__box__copy} >
+          <Link href='/' className={Style.footer__box__copy__link} >
+            <Image src={mensch} alt="Grupo Mensch" className={Style.footer__box__copy__link__logo} />
+          </Link>
+          <p className={Style.footer__box__copy__text} >Todos os direitos reservados || &copy; 2025 Grupo Mensch</p>
+        </div>
+        <div className={Style.footer__box__donuts} >
+
+          <Link href={'https://www.donutstech.com.br/'} target='_blank' className={Style.footer__box__donuts__link} >
+            <Image src={donuts} alt="Donutstech" className={Style.footer__box__donuts__link__logo} />
+          </Link>
+          <p className={Style.footer__box__donuts__text} > Feito com muito amor e açucar</p>
+        </div>
+      </div>
+      <div className={Style.footer__detalhe} />
+    </footer>
+  )
+};
+
+export default Footer;
