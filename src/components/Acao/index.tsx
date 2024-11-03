5a56652022eed8765a8240938ac389199a5d6cb9
Neto Russo <netoacrn1@gmail.com>
2024-11-03 03:49:44 -0300

    Alterando o titulo de ações

--- src/components/Acao/index.tsx
+++ src/components/Acao/index.tsx
@@ -11,7 +11,7 @@ const Acao = () => {
   return (
     <section className={Style.acao} aria-label='Ações sociais promovidas pelo Grupo Mensch'>
       <div className={Style.acao__head}>
-        <h1 className={Style.acao__head__titulo}><span>Transformando Vidas</span>, esse é o nosso Compromisso de <span>Servir</span> e <span>Impactar</span>.</h1>
+        <h1 className={Style.acao__head__titulo}><span>Impactando Vidas</span>, esse é o nosso Compromisso de <span>Servir</span> e <span>Transformar</span>.</h1>
         <p className={Style.acao__head__texto}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, laboriosam! Distinctio mollitia voluptate iure temporibus, ullam exercitationem voluptatem, amet quasi repellat, esse obcaecati libero ratione aliquid? Placeat dolores numquam corporis?</p>
       </div>
       {/* <div className={Style.acao__container}> */}
