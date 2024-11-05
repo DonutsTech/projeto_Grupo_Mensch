9b84fe5aac7fabc62365a4ab2d3170203da31916
Neto Russo <netoacrn1@gmail.com>
2024-11-05 11:48:03 -0300

    Alterando o banner solar!

--- src/components/BannerSolar/index.tsx
+++ src/components/BannerSolar/index.tsx
@@ -3,24 +3,27 @@
 
 import Image from 'next/image';
 import Style from './BannerSolar.module.scss';
-import logo from './assets/mesch_solar.svg';
+import logo from './assets/energiaSolar_white.svg';
 
 
 const BannerSolar = () => {
 
   return (
     <div className={Style.banner}>
-    <div className={Style.banner__overlay} />
-    <div className={Style.banner__content}>
-    <Image src={logo} alt="Mensh Energia Solar" className={Style.banner__content__logo} />
-    <h1 className={Style.banner__content__title}>
-      Uma Jornada de <span>Servir</span> e <span>Transformar</span>.
-    </h1>
-    </div>
-    <video  autoPlay loop muted className={Style.banner__video} >
-      <source src="/assets/videos/video_solar.mp4" type="video/mp4" />
-    </video>
-    
+      <div className={Style.banner__overlay} />
+      <div className={Style.banner__content}>
+        <Image src={logo} alt="Mensh Energia Solar" className={Style.banner__content__logo} />
+        <h1 className={Style.banner__content__title}>
+        Transforme despesas em <span>economia </span>e <span>conforto</span>.
+        </h1>
+        <button className={Style.banner__content__button}>
+          Invista no sol, economize dinheiro
+        </button>
+      </div>
+      <video autoPlay loop muted className={Style.banner__video} >
+        <source src="/assets/videos/video_solar.mp4" type="video/mp4" />
+      </video>
+
     </div>
   )
 };
