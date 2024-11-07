4ab89f707f40bec1c3aa64320c54a07a87ce1631
Neto Russo <netoacrn1@gmail.com>
2024-11-07 18:07:27 -0300

    finalizando a primeira parte de serviços

---
+++ src/components/GaleriaEnergia/index.tsx
@@ -0,0 +1,135 @@
+'use client';
+
+import Image from 'next/image';
+import Style from './GaleriaEnergia.module.scss';
+
+import pin from './assets/pin_energia.svg';
+
+import residencia from './assets/residencia.jpg'
+import seloResidencia from './assets/selo_residencia.svg';
+
+import empresa from './assets/empresa.jpg';
+import seloEmpresa from './assets/selo_negocio.svg';
+
+import rural from './assets/rural.jpg';
+import seloRural from './assets/selo_rural.svg';
+
+import { useEffect, useState } from 'react';
+
+const GaleriaEnergia = () => {
+  const [estado, setEstado] = useState(1);
+
+  useEffect(() => {
+
+    const timeEstado = setTimeout(() => {
+      if (estado === 1) {
+        setEstado(2);
+      } else if (estado === 2) {
+        setEstado(3);
+      } else if (estado === 3) {
+        setEstado(1);
+      }
+    }, 7000);
+
+    return () => clearTimeout(timeEstado);
+
+  }, [estado])
+
+
+
+
+
+  return (
+    <div className={Style.galeria}>
+      {estado === 1 && (
+        <div className={Style.galeria__content}>
+          <Image src={seloResidencia} alt="Selo com energia solar da Mensch" className={Style.galeria__content__selo} />
+          <Image src={residencia} alt="Residencia com energia solar da Mensch" className={Style.galeria__content__img} />
+
+          <div className={Style.galeria__content__ficha}>
+            <h3 className={Style.galeria__content__ficha__titulo}>
+              Para a sua <span>Casa</span>.
+            </h3>
+            <p className={Style.galeria__content__ficha__texto}>
+              Economize 90% na sua conta de energia!
+            </p>
+            <ul className={Style.galeria__content__ficha__lista}>
+              <li className={Style.galeria__content__ficha__lista__item}>
+                <Image src={pin} alt="Pin" className={Style.galeria__content__ficha__lista__item__pin} />
+                Economia Garantida.
+              </li>
+              <li className={Style.galeria__content__ficha__lista__item}>
+                <Image src={pin} alt="Pin" className={Style.galeria__content__ficha__lista__item__pin} />
+                Vida útil +25 anos.
+              </li>
+              <li className={Style.galeria__content__ficha__lista__item}>
+                <Image src={pin} alt="Pin" className={Style.galeria__content__ficha__lista__item__pin} />
+                Valorização do imóvel.
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+      {estado === 2 && (
+        <div className={Style.galeria__content}>
+          <Image src={seloEmpresa} alt="Selo com energia solar da Mensch" className={Style.galeria__content__selo} />
+          <Image src={empresa} alt="Empresa com energia solar da Mensch" className={Style.galeria__content__img} />
+
+          <div className={Style.galeria__content__ficha}>
+            <h3 className={Style.galeria__content__ficha__titulo}>
+              Para sua <span>Empresa</span>.
+            </h3>
+            <p className={Style.galeria__content__ficha__texto}>
+              Reduza Custos e Aumente a Eficiência
+            </p>
+            <ul className={Style.galeria__content__ficha__lista}>
+              <li className={Style.galeria__content__ficha__lista__item}>
+                <Image src={pin} alt="Pin" className={Style.galeria__content__ficha__lista__item__pin} />
+                Redução de Despesas Operacionais.
+              </li>
+              <li className={Style.galeria__content__ficha__lista__item}>
+                <Image src={pin} alt="Pin" className={Style.galeria__content__ficha__lista__item__pin} />
+                Energia Confiável.
+              </li>
+              <li className={Style.galeria__content__ficha__lista__item}>
+                <Image src={pin} alt="Pin" className={Style.galeria__content__ficha__lista__item__pin} />
+                Responsabilidade Ambiental.
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+      {estado === 3 && (
+        <div className={Style.galeria__content}>
+          <Image src={seloRural} alt="Selo com energia solar da Mensch" className={Style.galeria__content__selo} />
+          <Image src={rural} alt="Fazenda com energia solar da Mensch" className={Style.galeria__content__img} />
+
+          <div className={Style.galeria__content__ficha}>
+            <h3 className={Style.galeria__content__ficha__titulo}>
+              Para sua <span>Fazenda</span>.
+            </h3>
+            <p className={Style.galeria__content__ficha__texto}>
+              Aproveite o sol para uma fazenda sustentável.
+            </p>
+            <ul className={Style.galeria__content__ficha__lista}>
+              <li className={Style.galeria__content__ficha__lista__item}>
+                <Image src={pin} alt="Pin" className={Style.galeria__content__ficha__lista__item__pin} />
+                Tecnologia de Ponta.
+              </li>
+              <li className={Style.galeria__content__ficha__lista__item}>
+                <Image src={pin} alt="Pin" className={Style.galeria__content__ficha__lista__item__pin} />
+                Conservação do Ambiente.
+              </li>
+              <li className={Style.galeria__content__ficha__lista__item}>
+                <Image src={pin} alt="Pin" className={Style.galeria__content__ficha__lista__item__pin} />
+                Economia em Combustíveis Fósseis.
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+};
+
+export default GaleriaEnergia;
--- src/components/ServicosEnergia/index.tsx
+++ src/components/ServicosEnergia/index.tsx
@@ -1,11 +1,48 @@
 
+import Image from 'next/image';
 import Style from './ServicosEnergia.module.scss';
 
-const ServicosEnergia = () => { 
+import logo from './assets/mensch_solar.svg';
+import GaleriaEnergia from '../GaleriaEnergia';
+import Link from 'next/link';
+
+const ServicosEnergia = () => {
 
   return (
-    <section className={Style.section}>
-
+    <section className={Style.servicos}>
+      <div className={Style.servicos__head}>
+        <h2 className={Style.servicos__head__titulo}>
+          Uma Jornada <br />de <span>Servir</span> e <br /><span>Transformar</span>.
+        </h2>
+        <Image src={logo} alt="Mensh Energia Solar" className={Style.servicos__head__logo} />
+      </div>
+      <div className={Style.servicos__content}>
+        <div className={Style.servicos__content__textos}>
+          <p className={Style.servicos__content__textos__paragrafo}>
+            Descubra a solução completa para economizar energia e reduzir custos com a Mensch Energia Solar, especialista em Energia Solar Fotovoltaica e líder em inovação e sustentabilidade.
+          </p>
+          <p className={Style.servicos__content__textos__paragrafo}>
+            Nossa jornada começou com uma visão: transformar o futuro com energia solar. Agora, nossa paixão inspira mudanças reais. Junte-se a nós nessa jornada transformadora rumo a um futuro mais sustentável, limpo e próspero para as próximas gerações.
+          </p>
+          <p className={Style.servicos__content__textos__paragrafo}>
+            No Grupo Mensch, o sucesso vai além do lucro financeiro. Nossa missão é transformar vidas e comunidades por meio de soluções inovadoras e sustentáveis, gerando um impacto positivo na sociedade.
+          </p>
+          <p className={Style.servicos__content__textos__paragrafo}>
+            Com pesquisa e análise rigorosas, superamos desafios complexos e criamos um futuro melhor para todas as gerações. Nossa determinação é clara: fazer a diferença no mundo ao nosso redor, inspirando um legado de sustentabilidade e inovação.
+          </p>
+        </div>
+        <div className={Style.servicos__content__galeria}>
+          <GaleriaEnergia />
+        </div>
+      </div>
+      <div className={Style.servicos__cta}>
+        <p className={Style.servicos__cta__texto}>
+          Faça sua simulação e veja como podemos ajudar a reduzir seus gastos!
+        </p>
+        <Link className={Style.servicos__cta__btn} href={'/energiasolar'}>
+          Simule Agora
+        </Link>
+      </div>
     </section>
   )
 };
