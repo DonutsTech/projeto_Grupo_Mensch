ba85ea1939c68633c731d343536f7bd7f1f7c8f4
Milena Leme <milenaleme4@hotmail.com>
2024-11-12 17:18:58 -0300

    nova função de mensagem para solar

--- src/util/mensagem.ts
+++ src/util/mensagem.ts
@@ -14,3 +14,18 @@ export function mensagemGrupoMensch(formDados: FormContatoMascher) {
     Por favor, entre em contato com o remetente para mais informações, caso necessário.
   `
 }
+
+export function mensagemMenschSolar(formDados: FormContatoMascher) {
+  return `
+    Prezado(a) Administrador(a),
+
+    Você recebeu uma nova mensagem através do formulário de contato do Mensch Solar. Abaixo estão os detalhes enviados pelo remetente:
+
+      Nome: ${formDados.nome}
+      Email: ${formDados.email === undefined || formDados.email.trim() === '' ? '----- Contato Não informou ----- ' : formDados.email }
+      Telefone: ${formDados.telefone}
+      Mensagem:  ${formDados.mensagem === undefined || formDados.mensagem.trim() === '' ? '----- Contato Não informou ----- ' : formDados.email }
+
+    Por favor, entre em contato com o remetente para mais informações, caso necessário.
+  `
+}
