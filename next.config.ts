90d25abb295b1ab46ea51415f13bc33e39dffde1
Milena Leme <milenaleme4@hotmail.com>
2024-11-17 01:59:05 -0300

    config de nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,19 @@
 import path from "path";
 //@ts-expect-error @ts-ignore
 import withVideos from "next-videos";
+import { NextConfig } from "next";
 // import sass from "sass";
 
 /**@type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   /* config options here */
   sassOptions: {
     includePaths: [path.join(process.cwd(), './src/styles')],
+    silenceDeprecations: ["legacy-js-api"],
   },
   images: {
-    domains: [],
+    domains: ['prdcuscmscdn01.azureedge.net'],
   },
-
-  
 };
 
 export default withVideos(nextConfig, {
