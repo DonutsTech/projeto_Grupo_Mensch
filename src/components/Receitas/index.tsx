b40812401ddc566c4a4336deb6da1dfbdd9adfd8
Milena Leme <milenaleme4@hotmail.com>
2024-11-17 12:54:54 -0300

    inserindo novas funções

--- src/components/Receitas/index.tsx
+++ src/components/Receitas/index.tsx
@@ -1,6 +1,9 @@
-import Style from './Receitas.module.scss';
-import tipos from '@/util/tipos.json';
-import Image from 'next/image';
+import Style from "./Receitas.module.scss";
+import tipos from "@/util/tipos.json";
+import receitas from '@/util/receitas.json';
+import Image from "next/image";
+import { useState } from "react";
+import Modal from "react-modal";
 
 interface Tipo {
   id: number;
@@ -8,19 +11,117 @@ interface Tipo {
   imagem: string;
 }
 
-const Receitas = () => {
-  return (
-    <section className={Style.receitas}>
-      {
-        tipos.map(({id, imagem, nome}: Tipo) => (
-          <button type='button' key={id}>
-            <Image src={imagem} alt={nome} width={80} height={80}/>
-            <h3>{nome}</h3>
-          </button>
-        ))
-      }
-    </section>
-  )
+interface Receita {
+  id: number;
+  titulo: string;
+  subtitulo: string;
+  image: string;
+  tempo_de_preparo: number;
+  dificuldade: number;
+  porcoes: number;
+  Ingredientes: string[];
+  preparacao: string[];
+  tipo: number;
 }
 
+const Receitas = () => {
+  const [openReceitas, setOpenReceitas] = useState<boolean>(false);
+  const [openReceita, setOpenReceita] = useState<boolean>(false);
+  const [idTipo, setIdTipo] = useState<number>(0);
+  const [idReceita, setIdReceita] = useState<number>(0);
+
+  const toggleModal = () => {
+    setOpenReceitas(!openReceitas);
+  };
+
+  const toggleModalReceita = () => {
+    setOpenReceita(!openReceita);
+  };
+
+  return (
+    <>
+      <section className={Style.receitas}>
+        <div className={Style.receitas__tipos}>
+          {tipos.map(({ id, imagem, nome }: Tipo) => (
+            <div key={id} onClick={() => { setIdTipo(id); toggleModal(); }}>
+              <Image src={imagem} alt={nome} width={80} height={80} />
+              <h3>{nome}</h3>
+            </div>
+          ))}
+        </div>
+      </section>
+      <Modal
+        contentLabel="ModalReceitas"
+        isOpen={openReceitas}
+        ariaHideApp={false}
+        onRequestClose={toggleModal}
+        className={Style.modal}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(26, 34, 40, 0.8)",
+            zIndex: "30",
+          },
+        }}
+      >
+        {
+          !(idTipo === 0) && (
+            <>
+              {
+                receitas.filter((dado): dado is Receita => dado.tipo !== undefined && dado.tipo === idTipo).map(({image, titulo, id}, index) => (
+                  <div key={index} onClick={() => {toggleModalReceita(); setIdReceita(id);}}>
+                    <Image src={image} alt={titulo} width={80} height={80} />
+                    <h3>{titulo}</h3>
+                  </div>
+                ))
+              }
+            </>
+          )
+        }
+      </Modal>
+      <Modal
+        contentLabel="ModalReceita"
+        isOpen={openReceita}
+        ariaHideApp={false}
+        className={Style.modal}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(26, 34, 40, 0.8)",
+            zIndex: "31",
+          },
+        }}
+      >
+        {
+          idReceita !== 0 && (
+            <>
+              {
+                receitas.filter((dado): dado is Receita => dado.id !== undefined && dado.id === idReceita).map(({titulo, subtitulo, id}) => (
+                  <div key={id}>
+                    <h1>{titulo}</h1>
+                    <span>{subtitulo}</span>
+                  </div>
+                ))
+              }
+            </>
+          )
+        }
+
+      </Modal>
+    </>
+  );
+};
+
 export default Receitas;
+
+/*
+ {
+    "titulo": "",
+    "subtitulo": "",
+    "image": "",
+    "tempo_de_preparo": 0,
+    "dificuldade": 0,
+    "porcoes": 0,
+    "Ingredientes": [],
+    "preparacao": []
+  }
+
+*/
