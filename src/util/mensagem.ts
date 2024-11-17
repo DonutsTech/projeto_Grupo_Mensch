9bdb421f8774c3d9689deba3abf3ce5236ae0d41
Milena Leme <milenaleme4@hotmail.com>
2024-11-17 01:57:52 -0300

    modificando funções

--- src/util/mensagem.ts
+++ src/util/mensagem.ts
@@ -1,4 +1,4 @@
-import { FormContatoMascher } from "@/types";
+import { Endereco, FormContatoMascher, FormSimulacaoSolar } from "@/types";
 
 export function mensagemGrupoMensch(formDados: FormContatoMascher) {
   return `
@@ -25,6 +25,27 @@ export function mensagemMenschSolar(formDados: FormContatoMascher) {
       Email: ${formDados.email === undefined || formDados.email.trim() === '' ? '----- Contato Não informou ----- ' : formDados.email }
       Telefone: ${formDados.telefone}
       Mensagem:  ${formDados.mensagem === undefined || formDados.mensagem.trim() === '' ? '----- Contato Não informou ----- ' : formDados.email }
+
+    Por favor, entre em contato com o remetente para mais informações, caso necessário.
+  `
+}
+
+export function mensagemMenschSolarCalcular(endereco: Endereco ,formDados: FormSimulacaoSolar) {
+  return `
+    Prezado(a) Administrador(a),
+
+    Você recebeu uma nova mensagem através do formulário de Simulação da Mensch Solar. Abaixo estão os detalhes enviados pelo remetente:
+
+      Nome: ${formDados.nome}
+      Telefone: ${formDados.telefone}
+
+      Endereço de Cliente:
+        Rua: ${endereco.rua}
+        Bairro: ${endereco.bairro}
+        Cidade: ${endereco.estado} / ${endereco.uf}
+        CEP: ${endereco.cep}
+
+      O resultado da simulação: ${formDados.valor}
 
     Por favor, entre em contato com o remetente para mais informações, caso necessário.
   `
