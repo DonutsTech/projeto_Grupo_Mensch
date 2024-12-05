2726eab2ed03f8cd7df847efc8098859b63a421c
Milena Leme <milenaleme4@hotmail.com>
2024-12-05 12:09:19 -0300

    inserindo o meta do facebook

--- src/app/health/layout.tsx
+++ src/app/health/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "../globals.css";
 import Head from "next/head";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
+import Image from "next/image";
 // import Menu from "@/components/Menu";
 // import Footer from "@/components/Footer";
 
@@ -44,9 +46,32 @@ export default function RootLayout({
       <Head>
         <link rel="icon" href="./favicon.ico" type="image/x-icon" sizes="32x32" />
       </Head>
+      <Script id='fb-pixel' strategy='afterInteractive'>
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '937219011162733');
+          fbq('track', 'PageView');
+        `}
+      </Script>
       {/* <Menu page={'about'} /> */}
       {children}
       {/* <Footer /> */}
+      <noscript>
+        <Image
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=937219011162733&ev=PageView&noscript=1"
+          alt={'facebook pixel no script image'}
+        />
+      </noscript>
       <GoogleAnalytics gaId="G-Z2SYV3CJPP" />
     </>
   );
