3d308b4400b5f7923b233135321acfb8f628e2ef
Milena Leme <milenaleme4@hotmail.com>
2024-12-05 12:11:09 -0300

    inserindo sobre o meta do grupo masch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 // import Menu from "@/components/Menu";
 import Footer from "@/components/Footer";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "|| Grupo Mensch",
@@ -39,11 +41,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+      <Script id='fb-pixel' strategy='afterInteractive'>
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '555609490731974');
+          fbq('track', 'PageView');
+        `}
+      </Script>
       <body>
         {/* <Menu page={''}/> */}
         {children}
         <Footer />
         <GoogleAnalytics gaId="G-FE9N7BP8ZW" />
+        <noscript>
+          <Image height="1" width="1" style={{ display: 'none' }} src="https://www.facebook.com/tr?id=555609490731974&ev=PageView&noscript=1"  alt={'facebook pixel no script image'} />
+        </noscript>
       </body>
     </html>
   );
